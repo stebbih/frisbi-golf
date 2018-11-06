6345cf0eb6aa666bd296d2bcaf95e8beb5663af0
Anna Kristin <annah14@ru.is>
2018-11-06 16:47:57 +0000

    GameScreen komið lengra, þarf að tengja það við redux

--- components/Styles.js
+++ components/Styles.js
@@ -14,12 +14,6 @@ export default StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  horizontalViewTest: {
-    flex: 1,
-    width: Dimensions.get('window').width,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   textInputStyle: {
     height: 30,
     minWidth: 150,
@@ -35,5 +29,47 @@ export default StyleSheet.create({
     // alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
+  },
+  /** Game screen custom styles */
+  gameScreenBasketContainer: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#999',
+    backgroundColor: '#fbfbfb',
+    width: Dimensions.get('window').width - 8,
+    alignItems: 'center',
+  },
+  gameScreenPlayerContainer: {
+    flex: 1,
+    width: '100%',
+    padding: 10,
+    borderWidth: 2,
+    borderColor: '#fab',
+    backgroundColor: '#fbfbfb',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  gameScreenPlayerText: {
+    fontSize: 20,
+  },
+  gameScreenBasketScrollView: {
+    flex: 1,
+    width: '100%',
+  },
+  gameScreenHeaderContainer: {
+    paddingTop: 20,
+    padding: 10,
+  },
+  gameScreenHeaderText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  scoreInputStyle: {
+    height: 40,
+    width: 50,
+    borderBottomWidth: 1,
+    borderBottomColor: '#fab',
+    fontSize: 24,
   },
 });
