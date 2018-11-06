1cd9b867f2c0cfb3bf5c711547143ddbf373384d
sandransigurdar <sandransigurdar@gmail.com>
2018-11-06 12:53:57 +0000

    Fikt i NewGameScreen og bara syntax fix i ResultScreen

--- components/Styles.js
+++ components/Styles.js
@@ -1,71 +1,26 @@
 import { StyleSheet } from 'react-native';
-import { Constants } from 'expo';
 
-export default (styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    backgroundColor: '#fff',
-    marginTop: Constants.statusBarHeight
-  },
+export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center'
-  },
-  contentContainer: {
-    paddingTop: 30
-  },
-  welcomeContainer: {
+    backgroundColor: '#fff',
     alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20
+    justifyContent: 'center',
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10
+  textInputStyle: {
+    height: 30,
+    minWidth: 150,
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row',
+    fontSize: 15,
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50
+  /* *** New Game custom styles *** */
+  newGameContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    // alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
   },
-  homeScreenFilename: {
-    marginVertical: 7
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)'
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center'
-  },
-  navigationFilename: {
-    marginTop: 5
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center'
-  },
-  helpLink: {
-    paddingVertical: 15
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7'
-  }
-}));
+});
--- screens/NewGameScreen.js
+++ screens/NewGameScreen.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import {
-  StyleSheet,
   Text,
   View,
+  TextInput,
+  Button,
 } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
+import FontAwesome, { Icons } from 'react-native-fontawesome';
+import styles from '../components/Styles';
 
-});
 
+// Gera array af userum og senda a GameScreen
 
 export default class NewGameScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.newGameContainer}>
         <Text>New Game Screen</Text>
+        <TextInput style={styles.textInputStyle} />
+        {/* <FontAwesome>{Icons.plus-square-o}</FontAwesome> */}
+        <Button title="Spila!" />
       </View>
     );
   }
