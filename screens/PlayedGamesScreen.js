ec7ec88c54950dd24eba3c2f009f8fe17c3c908c
birna14 <birna14@ru.is>
2018-11-08 13:48:51 +0000

    Update PlayedGamesScreen.js

--- screens/PlayedGamesScreen.js
+++ screens/PlayedGamesScreen.js
@@ -5,17 +5,8 @@ import Colors from '../constants/Colors';
 
 export default class GameScreen extends React.Component {
   static navigationOptions = {
-    title: 'LEIKIR',
-    headerLeft: (<View />),
-    headerStyle: {
-      backgroundColor: Colors.tintColor,
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontSize: 16,
-      alignSelf: 'center',
-    },
-  };
+    header: null,
+  };	 
 
   render() {
     return (
