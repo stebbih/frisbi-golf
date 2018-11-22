9407bc75f2f5fc180575c6c908cd6ffa4688373e
Anna Kristin <annah14@ru.is>
2018-11-22 12:32:07 +0000

    setja key á leiki

--- redux/reducers/gameStorageReducer.js
+++ redux/reducers/gameStorageReducer.js
@@ -3,7 +3,7 @@ import { SAVE_GAME } from '../constants/gameStorageConstants';
 const gameStorageReducer = (state = [], action) => {
   switch (action.type) {
     case SAVE_GAME:
-      return [action.payload, ...state];
+      return [{ ...action.payload, key: `${state.length}` }, ...state];
     default:
       return state;
   }
